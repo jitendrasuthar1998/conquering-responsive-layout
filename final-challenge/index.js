9d6c6ae6d1bdb4eda291e3c4ff65e396b6d78260
Jitendra Suthar <jitendrasuthar995@gmail.com>
2023-11-14 01:37:01 +0530

    wip

---
+++ final-challenge/index.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.navbar');
+
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+})
